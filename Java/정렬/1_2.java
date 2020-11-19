6084b26407d52fd864432db5f72b742fbed5793f
repl.it user <replituser@example.com>
2020-11-19 03:35:03 +0000

    삽입 정렬

---
+++ Java/정렬/1_2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    
+    int n= 10;
+    int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+    for(int i=1; i<n; i++){
+      // 인덱스 i부터 1까지 감소
+      for(int j=i; j>0; j--){
+        // 왼쪽으로 이동하면서 비교 
+        if(arr[j] < arr[j-1]){
+          int tmp = arr[j];
+          arr[j] = arr[j-1];
+          arr[j-1] = tmp;
+        }
+        else break;
+      }
+    }
+
+    for(int i=0; i<n; i++){
+      System.out.print(arr[i] + " ");
+    }
+  }
+}
