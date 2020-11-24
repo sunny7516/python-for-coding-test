17cae715d9eb897a424101b31c70e427415126cb
repl.it user <replituser@example.com>
2020-11-24 06:44:18 +0000

    부품찾기_계수 정ㄹ려

---
+++ Java/이진 탐색/2_2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+// 부품 찾기_계수 정렬
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int n = scan.nextInt();
+    int[] nrr = new int[1000001];
+    for(int i=0; i<n; i++){
+      int x = scan.nextInt();
+      nrr[x] = 1;
+    }
+
+    int m = scan.nextInt();
+    int[] mrr = new int[m];
+    for(int i=0; i<m; i++){
+      mrr[i] = scan.nextInt();
+    }
+
+    for(int i=0; i<m; i++){
+      if(nrr[mrr[i]] == 1){
+        System.out.print("yes ");
+      }else{
+        System.out.print("no ");
+      }
+    }
+  }
+}
