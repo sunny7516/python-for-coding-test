861fcb45f387db2767f8425be70e4e02a094f7ad
repl.it user <replituser@example.com>
2020-11-24 05:00:19 +0000

    이진 탐색

---
+++ Java/이진 탐색/1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main {
+
+  // 순차 탐색 소스코드 구현
+  public static int sequantialSearch(int n, String str, String[] arr){
+    for(int i=0; i<n; i++){
+      if(str.equals(arr[i])){ // 같은 문자열 찾으면 
+        return i+1; // 인덱스 +1 출력(문자열 위치)
+      }
+    }
+      return -1; // 찾지 못한 경우
+  }
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("생성할 원소 개수를 입력한 다음 한 칸 띄고 찾을 문자열을 입력하세요.");
+    int n = scan.nextInt(); // 원소 개수
+    String str = scan.next(); // 찾을 문자열
+
+    String[] arr = new String[n];
+    System.out.println("앞서 적은 원소 개수만큼 문자열을 입력하세요. 구분은 띄어쓰기 한 칸으로 합니다.");
+    for(int i=0; i<n; i++){
+      arr[i] = scan.next();
+    }
+
+    // 순차 탐색 결과
+    System.out.println(sequantialSearch(n, str, arr));
+   
+  }
+}
---
+++ Java/이진 탐색/1_2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Main {
+
+  public static int binarySearch(int start, int end, int target, int[] arr){
+    if(start > end) return -1;
+    int check = (start + end) / 2;
+    // 찾은 경우
+    if(arr[check] == target){ 
+      // 인덱스 반환
+      return check; 
+    // 중간값보다 찾는 값이 큰 경우 
+    }else if(arr[check] < target){
+      // 오른쪽 확인
+      return binarySearch(check+1, end, target, arr);
+    }else{ // 중간값보다 찾는 값이 작은 경우
+      // 왼쪽 확인 
+      return binarySearch(start, check-1, target, arr);
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int n = scan.nextInt();
+    int target = scan.nextInt();
+
+    int[] arr = new int[n];
+    for(int i=0; i<n; i++){
+      arr[i] = scan.nextInt();
+    }
+
+    int result = binarySearch(0, n-1, target, arr);
+    if(result == -1){
+      System.out.println("원소가 존재하지 않습니다.");
+    }else{
+      System.out.println(result + 1);
+    }
+  }
+}
