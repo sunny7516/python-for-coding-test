038ee569615847e67f0c9b23eca4f035668d63e9
Suna Hong <limbo001@naver.com>
2021-01-20 21:14:11 +0900

    삽입정렬

--- Java/정렬/1_2.java
+++ Java/정렬/1_2.java
@@ -1,28 +1,30 @@
 import java.util.*;
 
 class Main {
-  // 삽입정렬 
   public static void main(String[] args) {
-    
     int n= 10;
     int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
 
+    // 두번째 값부터 확인
     for(int i=1; i<n; i++){
-      // 인덱스 i부터 1까지 감소
+      // 기준값보다 앞에 있는 인덱스만 확인
       for(int j=i; j>0; j--){
-        // 왼쪽으로 이동하면서 비교 
         if(arr[j] < arr[j-1]){
-          // 작으면 교체 
+          // 기준보다 작은 값 스와프 
           int tmp = arr[j];
           arr[j] = arr[j-1];
           arr[j-1] = tmp;
+        }else{
+          // 작은 값이 나오면
+          // 그 앞부분은 이미 정렬됨
+          break;
         }
-        else break;
       }
     }
 
     for(int i=0; i<n; i++){
       System.out.print(arr[i] + " ");
     }
+
   }
 }
